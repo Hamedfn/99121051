1e848d23c654c485c7a4c7f9406f2670da31be02
Hamedfn <farajian_h@yahoo.com>
2023-05-06 14:15:39 +0430

    404veiw

--- test.php
+++ test.php
@@ -8,6 +8,7 @@ $book -> Author = 'Author 1';
 dd($book);
 */
 
+
 $dsn ='mysql:host=localhost;port=3306;dbname=web2;charset=utf8mb4'; 
 $pdo = new PDO ($dsn,'root');
 $statement = $pdo -> prepare ('SELECT * FROM book') ;
